be18988ddb5db584bb523a20c74a5af2e7a703fe
Thongkorn Tubtimkrob <thongkorn@hotmail.com>
2021-07-28 15:43:20 +0700

    Add files via upload

---
+++ LineNotifyUploadImage/LineNotifyUploadImage/frmLineNotify.designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLineNotify
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmLineNotify))
+        Me.txtMessage = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.btnSend = New System.Windows.Forms.Button()
+        Me.picData = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnDeleteImg = New System.Windows.Forms.Button()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        CType(Me.picData, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtMessage
+        '
+        Me.txtMessage.Location = New System.Drawing.Point(83, 16)
+        Me.txtMessage.Multiline = True
+        Me.txtMessage.Name = "txtMessage"
+        Me.txtMessage.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtMessage.Size = New System.Drawing.Size(309, 186)
+        Me.txtMessage.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 14)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Message"
+        '
+        'btnClose
+        '
+        Me.btnClose.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue
+        Me.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnClose.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnClose.Image = CType(resources.GetObject("btnClose.Image"), System.Drawing.Image)
+        Me.btnClose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnClose.Location = New System.Drawing.Point(412, 63)
+        Me.btnClose.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(121, 39)
+        Me.btnClose.TabIndex = 2
+        Me.btnClose.Text = "Exit Program"
+        Me.btnClose.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'btnSend
+        '
+        Me.btnSend.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnSend.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue
+        Me.btnSend.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSend.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnSend.Image = CType(resources.GetObject("btnSend.Image"), System.Drawing.Image)
+        Me.btnSend.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnSend.Location = New System.Drawing.Point(412, 16)
+        Me.btnSend.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.Size = New System.Drawing.Size(121, 39)
+        Me.btnSend.TabIndex = 1
+        Me.btnSend.Text = "Send Data"
+        Me.btnSend.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnSend.UseVisualStyleBackColor = True
+        '
+        'picData
+        '
+        Me.picData.BackColor = System.Drawing.Color.Gainsboro
+        Me.picData.Image = CType(resources.GetObject("picData.Image"), System.Drawing.Image)
+        Me.picData.Location = New System.Drawing.Point(83, 209)
+        Me.picData.Name = "picData"
+        Me.picData.Size = New System.Drawing.Size(309, 340)
+        Me.picData.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picData.TabIndex = 12
+        Me.picData.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 209)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 14)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "Image"
+        '
+        'btnDeleteImg
+        '
+        Me.btnDeleteImg.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnDeleteImg.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnDeleteImg.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.btnDeleteImg.Location = New System.Drawing.Point(293, 555)
+        Me.btnDeleteImg.Name = "btnDeleteImg"
+        Me.btnDeleteImg.Size = New System.Drawing.Size(99, 26)
+        Me.btnDeleteImg.TabIndex = 72
+        Me.btnDeleteImg.Text = "Delete Image"
+        Me.btnDeleteImg.UseVisualStyleBackColor = True
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnBrowse.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnBrowse.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.btnBrowse.Location = New System.Drawing.Point(83, 555)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(99, 26)
+        Me.btnBrowse.TabIndex = 71
+        Me.btnBrowse.Text = "Select Image"
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'frmLineNotify
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 14.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(551, 591)
+        Me.Controls.Add(Me.btnDeleteImg)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.picData)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnSend)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtMessage)
+        Me.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "frmLineNotify"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Line Notify and Upload Image with VB.NET"
+        CType(Me.picData, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtMessage As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnClose As System.Windows.Forms.Button
+    Friend WithEvents btnSend As System.Windows.Forms.Button
+    Friend WithEvents picData As System.Windows.Forms.PictureBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnDeleteImg As System.Windows.Forms.Button
+    Friend WithEvents btnBrowse As System.Windows.Forms.Button
+
+End Class
---
+++ LineNotifyUploadImage/LineNotifyUploadImage/frmLineNotify.vb
@@ -0,0 +1,193 @@
+﻿' / --------------------------------------------------------------------------------
+' / Developer : Mr.Surapon Yodsanga (Thongkorn Tubtimkrob)
+' / eMail : thongkorn@hotmail.com
+' / URL: http://www.g2gnet.com (Khon Kaen - Thailand)
+' / Facebook: https://www.facebook.com/g2gnet (For Thailand)
+' / Facebook: https://www.facebook.com/commonindy (Worldwide)
+' / More Info: http://www.g2gsoft.com
+' /
+' / Purpose: Line Notify and upload images with VB.NET (2010)
+' / Microsoft Visual Basic .NET (2010)
+' /
+' / This is open source code under @Copyleft by Thongkorn Tubtimkrob.
+' / You can modify and/or distribute without to inform the developer.
+' / --------------------------------------------------------------------------------
+Imports System.Net
+Imports System.Text
+Imports System.IO
+
+'// ดาวน์โหลด cURL สำหรับ Windows ทั้ง 32 บิตและ 64 บิต
+'// https://curl.haxx.se/windows/
+
+Public Class frmLineNotify
+    Dim streamPic As Stream     '// Use Steam instead IO.
+    Dim PicturePath As String = MyPath(Application.StartupPath) & "Images\"
+    '//
+    Dim FullPathFileName As String = String.Empty
+    '// เปลี่ยน TOKEN ที่นี่ที่เดียว
+    Const strToken As String = "TOKEN"
+
+    Private Sub frmLineNotify_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        txtMessage.Text = "ทดสอบการส่ง Line Notify จากคุณทองก้อน ทับทิมกรอบ"
+        picData.Image = Image.FromFile(PicturePath & "NoImage.gif")
+    End Sub
+
+    Private Sub btnSend_Click(sender As System.Object, e As System.EventArgs) Handles btnSend.Click
+        If String.IsNullOrEmpty(txtMessage.Text.Trim) Then
+            MessageBox.Show("Nothing message to send.", "Report Status", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            Return
+        End If
+        Try
+            Cursor.Current = Cursors.WaitCursor
+            System.Net.ServicePointManager.Expect100Continue = False
+            Dim Request = DirectCast(WebRequest.Create("https://notify-api.line.me/api/notify"), HttpWebRequest)
+            '// Message to Line.
+            Dim LineMessage = String.Format("message={0}", txtMessage.Text & vbCrLf & "วันที่ - เวลา : " & FormatDateTime(Now(), DateFormat.GeneralDate))
+            '// หากต้องการส่ง Sticker ออกไปด้วย
+            '// List of available stickers ... https://developers.line.biz/en/docs/messaging-api/sticker-list/
+            LineMessage += "&stickerPackageId=1" & "&stickerId=109"
+
+            Dim MyData = Encoding.UTF8.GetBytes(LineMessage)
+            Request.Method = "POST"
+            '// Initialize
+            With Request
+                .ContentType = "application/x-www-form-urlencoded"
+                .ContentLength = MyData.Length
+                '// Change your Token and don't cut "Bearer".
+                .Headers.Add("Authorization", "Bearer " & strToken)
+                .AllowWriteStreamBuffering = True
+                .KeepAlive = False
+                .Credentials = CredentialCache.DefaultCredentials
+            End With
+            '//
+            Using Stream = Request.GetRequestStream()
+                Stream.Write(MyData, 0, MyData.Length)
+            End Using
+            Dim response = DirectCast(Request.GetResponse(), HttpWebResponse)
+            Dim responseString = New StreamReader(response.GetResponseStream()).ReadToEnd()
+
+            '//
+            If FullPathFileName.Trim <> "" Or FullPathFileName.Length <> 0 Then Call SendPicture()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            Cursor.Current = Cursors.Default
+            MessageBox.Show("ส่งข้อความและรูปภาพ Line Notify เรียบร้อย.", "รายงานสถานะ", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End Try
+    End Sub
+
+    Sub SendPicture()
+        '// Format
+        '// " -X POST -H "Authorization: Bearer TOKEN" -F "message=Send Picture" -F "imageFile=@D:\Sample.jpg" https://notify-api.line.me/api/notify"
+        Try
+            Dim arg As String = String.Empty
+            arg &= " -X POST -H "
+            arg &= """Authorization: Bearer " & strToken & """"
+            arg &= " -F ""message=" & "Send Picture" & """"
+            arg &= " -F ""imageFile=@" & FullPathFileName.Trim & """ https://notify-api.line.me/api/notify"
+            ShellandWait("curl.exe", arg)
+            '//
+            picData.Image = Image.FromFile(PicturePath & "NoImage.gif")
+            FullPathFileName = String.Empty
+        Catch ex As Exception
+            MessageBox.Show("Error", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Public Sub ShellandWait(ByVal ProcessPath As String, ByVal Arguments As String)
+        Dim objProcess As System.Diagnostics.Process
+        Try
+            objProcess = New System.Diagnostics.Process()
+            objProcess.StartInfo.Arguments = Arguments
+            objProcess.StartInfo.FileName = ProcessPath
+            objProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+            objProcess.Start()
+            Application.DoEvents()
+            objProcess.WaitForExit()
+            Application.DoEvents()
+            Console.WriteLine(objProcess.ExitCode.ToString())
+            objProcess.Close()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnClose_Click(sender As System.Object, e As System.EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+
+    Private Sub frmLineNotify_FormClosed(sender As Object, e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        Me.Dispose()
+        GC.SuppressFinalize(Me)
+        Application.Exit()
+    End Sub
+
+    Private Sub btnBrowse_Click(sender As System.Object, e As System.EventArgs) Handles btnBrowse.Click
+        Dim dlgImage As OpenFileDialog = New OpenFileDialog()
+
+        ' / Open File Dialog
+        With dlgImage
+            '.InitialDirectory = PicturePath 'PicturePath
+            .Title = "Select your image file"
+            .Filter = "Image (*.jpg;*.png;*.gif;*.bmp)|*.jpg;*.png;*.gif;*.bmp"
+            .FilterIndex = 1
+            .RestoreDirectory = True
+        End With
+        '/ Select OK after Browse ...
+        If dlgImage.ShowDialog() = DialogResult.OK Then
+            FullPathFileName = dlgImage.FileName
+            picData.Image = Image.FromFile(FullPathFileName)
+        End If
+    End Sub
+
+    ' / -----------------------------------------------------------------------------
+    ' / Use Steam instead IO.
+    ' / -----------------------------------------------------------------------------
+    Sub ShowPicture(PicName As String)
+        Dim imgDB As Image
+        ' Get the name of the image file.
+        If PicName.ToString <> "" Then
+            ' Verify that the image file meets the specified location.
+            If System.IO.File.Exists(PicturePath & PicName.ToString) Then
+                '/ Because when deleting the image file is locked, it can not be removed.
+                '/ The file is closed after the image is loaded, so you can delete the file if you need.
+                streamPic = File.OpenRead(PicturePath & PicName.ToString)
+                imgDB = Image.FromStream(streamPic)
+                picData.Image = imgDB
+            Else
+                '/ No images.
+                streamPic = File.OpenRead(PicturePath & "NoImage.gif")
+                imgDB = Image.FromStream(streamPic)
+                picData.Image = imgDB
+            End If
+
+            ' Is null
+        Else
+            streamPic = File.OpenRead(PicturePath & "NoImage.gif")
+            imgDB = Image.FromStream(streamPic)
+            picData.Image = imgDB
+        End If
+        '//
+        streamPic.Dispose()
+    End Sub
+
+    ' / Get my project path
+    ' / AppPath = C:\My Project\bin\debug
+    ' / Replace "\bin\debug" with "\"
+    ' / Return : C:\My Project\
+    Function MyPath(ByVal AppPath As String) As String
+        '/ MessageBox.Show(AppPath);
+        AppPath = AppPath.ToLower()
+        '/ Return Value
+        MyPath = AppPath.Replace("\bin\debug", "\").Replace("\bin\release", "\").Replace("\bin\x86\debug", "\").Replace("\bin\x86\release", "\")
+        '// If not found folder then put the \ (BackSlash) at the end.
+        If Microsoft.VisualBasic.Right(MyPath, 1) <> Chr(92) Then MyPath = MyPath & Chr(92)
+    End Function
+
+    Private Sub btnDeleteImg_Click(sender As System.Object, e As System.EventArgs) Handles btnDeleteImg.Click
+        picData.Image = Image.FromFile(PicturePath & "NoImage.gif")
+        FullPathFileName = String.Empty
+    End Sub
+End Class
